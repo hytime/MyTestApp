f10bf0ca6d32246a83d806856e46ca08eddef64b
hy <254499600@qq.com>
2024-03-11 22:09:32 +0800

    chang menlist thpe function onPress

--- src/components/MenuList/type.ts
+++ src/components/MenuList/type.ts
@@ -2,9 +2,8 @@ export type ListMenuItem = {
   title: string;
   icon: string;
   onPress?:
-    | ((params: {[params: string]: any}) => any)
-    | ((item?: any, toast?: any) => Promise<any>)
-    | ((item?: any, toast?: any) => any)
+    | ((params: {[params: string]: any} | any) => any | Promise<any>)
+    | ((item?: any, toast?: any) => Promise<any> | any)
     | any;
 };
 export type MenuListType = {
