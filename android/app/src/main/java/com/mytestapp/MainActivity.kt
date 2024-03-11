b4e0e4eb8e947ead489570ca29699f2ddf7ee922
hy <254499600@qq.com>
2024-03-11 12:29:14 +0800

    add android realse key

--- android/app/src/main/java/com/mytestapp/MainActivity.kt
+++ android/app/src/main/java/com/mytestapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mytestapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,12 +12,12 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "MyTestApp"
+  override fun getMainComponentName(): String = "hymine"
   
-  @Override  
-  protected void onCreate(Bundle savedInstanceState) {  
-    super.onCreate(null);  
-  }  
+
+//  override fun onCreate( savedInstanceState: Bundle){
+//    super.onCreate(null);
+//  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
